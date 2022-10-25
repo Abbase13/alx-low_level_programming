bb62d7a4b7777d6e2936e53682161f03cd8e5f33
Abbase13 <abbaseawil13@gmail.com>
2022-10-25 13:47:18 -0700

    All commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * swap_int - swaps the valws of two integers
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string to given destination
+ * @dest: pointer parameter to copied it
+ * @src: pointer parameter copied from it.
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = 0;
+
+	while (*(src + length) != '\0')
+	{
+		*(dest + length) = *(src + length);
+		length++;
+	}
+
+	*(dest + length) = *(src + length);
+
+	return (dest);
+}
